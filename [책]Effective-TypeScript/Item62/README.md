93efb6b249d946eeefa69251da6267d9c54949c6
이준규 <jkjkjk5710@gmail.com>
2024-03-10 23:49:20 +0900

    쏙쏙 들어오는 함수형 코딩

--- [책]Effective-TypeScript/Item62/README.md
+++
@@ -1,3 +0,0 @@
-## 마이그레이션 완성을 위해 noImplicitAny 사용하기
-
-후에 strictNullCheck, strict true 
--- [책]nodejs-design-pattern-bible/Chapter05/spider-cli.js
+++
@@ -1,5 +0,0 @@
-import { spider } from "./spider.js";
-
-spider(url, nesting)
-  .then(() => console.log("download complete"))
-  .catch((err) => console.error(err));
